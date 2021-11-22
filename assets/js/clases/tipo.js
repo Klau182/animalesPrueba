582b8c57c8bb120656562fed49d6c464725bd71a
claudia <claudia_figueroai@hotmail.com>
2021-11-22 12:11:43 -0300

    creacion de la clase animal de donde se extenderan leon, lobo, oso, aguila y serpiente

---
+++ assets/js/clases/tipo.js
@@ -0,0 +1,57 @@
+import Animal from "./Animal.js";
+
+class Leon extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Rugir() {
+    return this.sonido;
+    
+  }
+}
+
+class Lobo extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Aullar() {
+    return this.sonido;
+  }
+}
+
+class Oso extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Grunir() {
+    return this.sonido;
+    
+  }
+}
+
+class Serpiente extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Sisear() {
+    return this.sonido;
+    
+  }
+}
+
+class Aguila extends Animal {
+  constructor(nombre, edad, img, comentarios, sonido) {
+    super(nombre, edad, img, comentarios, sonido);
+  }
+
+  Chillar() {
+    return this.sonido;
+    
+  }
+}
+
+export { Leon, Lobo, Oso, Serpiente, Aguila };
