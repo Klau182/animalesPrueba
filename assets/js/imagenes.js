74396bb30f2d6c4bab3f0839c96d1be455f41815
claudia <claudia_figueroai@hotmail.com>
2021-11-21 22:55:45 -0300

    funcion para buscar imagenes

--- assets/js/imagenes.js
+++ assets/js/imagenes.js
@@ -1,1 +1,11 @@
 import Animales from './Consulta.js'
+
+const animal = document.getElementById("animal")
+const preview = document.getElementById("preview")
+
+animal.addEventListener("click", async () => {
+    const { animales } = await Animales.getData()
+    const animalNombre = animal.value
+    const imagenPreview = animales.find(animal => animal.name === animalNombre)
+    preview.style.backgroundImage = `url("./assets/imgs/${imagenPreview.imagen}")`
+})
